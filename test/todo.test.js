cb31363c1dd085cf4398ab8fbabf2d841e5f44f5
Ednei R Louback <89962959+nei22@users.noreply.github.com>
2022-04-29 19:24:09 -0300

    Add files via upload

---
+++ test/todo.test.js
@@ -0,0 +1,57 @@
+const {describe, it, before, beforeEach, afterEach} = require('mocha')
+const { createSandbox } = require('sinon')
+const {expect} = require('chai')
+const Todo = require('../src/todo')
+
+describe('todo', () => {
+    let sandbox
+    beforeEach(() => {
+        sandbox = createSandbox()
+    })
+    afterEach(() => sandbox.restore())
+    describe('#isValid', () => {
+        it('should return invalid when creating an object without text', () => {
+            const data = {
+                text: '',
+                when: new Date("2022-04-28")
+            }
+            const todo = new Todo(data)
+            const result = todo.isValid()
+            expect(result).to.be.not.ok
+        })
+        it('should return invalid when creating an object using the "when" property invalid', () => {
+            const data = {
+                text: 'Hello world!!',
+                when: new Date("20-04-28")
+            }
+            const todo = new Todo(data)
+            const result = todo.isValid()
+            expect(result).to.be.not.ok
+            
+        })
+        it('should have "id", "text", "when" and "status" after creating an object', () =>{
+            const data = {
+                text: 'Hello world!!',
+                when: new Date("2022-04-28")
+            }
+
+            const expectedId = '000001'
+            
+            const uuid = require('uuid')
+            const fakeUUID = sandbox.fake.returns(expectedId)
+            sandbox.replace(uuid, "v4", fakeUUID)
+
+            const todo = new Todo(data)
+            const expectedItem = {
+                text: data.text,
+                when: data.when,
+                status: "",
+                id: expectedId
+            }
+            const result = todo.isValid()
+            expect(result).to.be.ok
+            expect(uuid.v4.calledOnce).to.be.ok
+            expect(todo).to.be.deep.equal(expectedItem)
+        })
+    })
+})
